ea23f14f0dc9117d9223dc9836918cdc96e541c5
Scholz, Marie <marie.scholz@tecalliance.net>
2022-09-15 11:01:42 +0200

    do while loop introduction

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -11,15 +11,23 @@ namespace beginning
         static void Main(string[] args)
         {
             int i = 0;
-            while(i< 10)
+            do
+
             {
                 Console.WriteLine(i);
                 i++;
-            }
-           
+
+            } while (i < 10);
+
+            while (i < 10)
+             Console.WriteLine(i);
+                i++;
+                
+              
+            
             
 
             Console.ReadLine();
-        }
+        }    
     }
 } 
