2115dac03730a087453341170b27b321d8dff786
Scholz, Marie <marie.scholz@tecalliance.net>
2022-09-15 10:35:15 +0200

    Add Trim method on strings

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -13,11 +13,10 @@ namespace beginning
             Console.WriteLine("Gib mir ein Wort");
             string entry = Console.ReadLine();
 
+           string trimmed_entry = entry.Trim(' ',  'a');
 
-            string replaced_entry = entry.Replace("U", "G").Replace("u", "g");
-
-            Console.WriteLine("Der alte Text war " + entry);
-            Console.WriteLine("Der neue Text ist " + replaced_entry);
+            Console.WriteLine("Old String " + entry + ":");
+            Console.WriteLine("New String " + trimmed_entry + ":");
 
 
             Console.ReadLine();
